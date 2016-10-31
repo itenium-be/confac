a0c1daf10630cb625bd4f5e777743a6f24d98599
Laoujin <woutervs@hotmail.com>
2016-10-31 01:58:42 +0100

    pug: finished invoice layout

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ import koa from 'koa';
 import koaRouter from 'koa-router';
 import koaCors from 'kcors';
 import koaBodyParser from 'koa-bodyparser';
+import koaServe from 'koa-static';
+
+import moment from 'moment';
+import numeral from 'numeral';
+numeral.language('nl', {
+  delimiters: {thousands: '.', decimal: ','},
+  currency: {symbol: '€'}
+});
+numeral.language('nl');
 
 const app = koa();
 app.use(koaCors());
 app.use(koaBodyParser());
+app.use(koaServe('./templates/'));
 
 const router = koaRouter();
 
-router.get('/', function *(next) {
-  this.body = 'Hello World!';
-});
+// router.get('/', function *(next) {
+//   this.body = 'Hello World!';
+// });
 
 router.get('/api/clients', function *(next) {
   this.body = [{
@@ -22,7 +32,8 @@ router.get('/api/clients', function *(next) {
     telephone: '054 / 80 45 03',
     btw: '0478.378.759',
     rate: {
-      hourly: 50
+      hourly: 50,
+      description: 'Crm',
     }
   }, {
     id: 2,
@@ -31,7 +42,8 @@ router.get('/api/clients', function *(next) {
     city: 'B-1800 Vilvoorde',
     btw: 'BE0478.895.136',
     rate: {
-      hourly: 65
+      hourly: 65,
+      description: 'PitStop',
     }
   }];
 });
@@ -55,6 +67,11 @@ const pongit = {
   email: 'woutervs@hotmail.com',
 }
 
+const locals = {
+  formatDate: dateString => moment(dateString).format('DD/MM/YYYY'),
+  numberFormat: number => numeral(number).format('0,0.00'),
+};
+
 const pug = require('pug');
 // app.set('view engine', 'pug');
 // app.set('views', './templates');
@@ -63,8 +80,9 @@ const compiledFunction = pug.compileFile('./templates/pongit.pug');
 router.get('/api/invoice/create', function *(next) {
   //const params = this.request.body;
   const params = {"client":{"id":2,"name":"Nexios Consulting Group NV","address":"Telecom Gardens – Medialaan 36","city":"B-1800 Vilvoorde","btw":"BE0478.895.136","rate":{"hourly":65}},"number":11,"date":"2016-10-31T22:59:59.999Z","hours":24,"money":{"totalWithoutTax":1560,"taxPercentage":21,"totalTax":327.59999999999997,"total":1887.6}}
+  //params.date = moment(params.date);
 
-  this.body = compiledFunction(Object.assign({your: pongit}, params));
+  this.body = compiledFunction(Object.assign({your: pongit}, locals, params, {origin: this.request.origin}));
 });
 
 app.use(router.routes());
