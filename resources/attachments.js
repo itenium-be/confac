4fe273e2e53b8b56ce90b3e0c40a3b86ca74e020
Laoujin <woutervs@hotmail.com>
2017-01-12 19:15:48 +0100

    invoice attachments: wip

--- resources/attachments.js
+++ resources/attachments.js
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import { htmlToBuffer, createHtml } from './invoices.js';
 
 export default function register(app) {
   const router = new Router({
@@ -10,6 +11,31 @@ export default function register(app) {
     const attachment = yield this.mongo.collection('attachments').findOne(invoiceId.toObjectId(), {[type]: 1});
     this.body = attachment.pdf;
   });
+
+  router.put('/:invoiceId/:type', function *() {
+    const {invoiceId, type} = this.params;
+    const params = yield this.mongo.collection('invoices').findOne(invoiceId.toObjectId());
+
+    const html = createHtml(params, this.request.origin);
+    const pdfBuffer = yield htmlToBuffer(html);
+
+    yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: pdfBuffer}});
+
+    this.body = 'ok';
+  });
+
+  router.put('/:invoiceId/:type', function *() {
+    const {invoiceId, type} = this.params;
+
+    console.log(invoiceId, type);
+
+    //const params = yield this.mongo.collection('invoices').findOne(invoiceId.toObjectId());
+
+    //yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: pdfBuffer}});
+
+    this.body = 'ok';
+  });
+
 
   app.use(router.routes());
   app.use(router.allowedMethods());
