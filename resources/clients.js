5e6bbf45c61bdd48b397f26ff0f309d7f790f76c
Laoujin <woutervs@hotmail.com>
2017-01-14 21:32:08 +0100

    insert/update client details

--- resources/clients.js
+++ resources/clients.js
@@ -9,6 +9,21 @@ export default function register(app) {
     this.body = yield this.mongo.collection('clients').find().toArray();
   });
 
+  router.post('/', function *() {
+    const {_id, ...params} = this.request.body;
+    if (_id) {
+      // console.log('updating', _id, params);
+      yield this.mongo.collection('clients').update(_id.toObjectId(), params);
+      this.body = this.request.body;
+
+    } else {
+      const inserted = yield this.mongo.collection('clients').insert(params);
+      // console.log('inserted', inserted);
+      const insertedId = inserted.insertedIds[1];
+      this.body = Object.assign(params, insertedId.toString().toObjectId());
+    }
+  });
+
   app.use(router.routes());
   app.use(router.allowedMethods());
 }
