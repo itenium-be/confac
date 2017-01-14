8f93285b34eb748db8904934bdda74da5b1d9b71
Laoujin <woutervs@hotmail.com>
2017-01-15 00:52:49 +0100

    client: save slug from name on creation

--- resources/clients.js
+++ resources/clients.js
@@ -1,4 +1,5 @@
 import Router from 'koa-router';
+import slugify from 'slugify';
 
 export default function register(app) {
   const router = new Router({
@@ -10,13 +11,15 @@ export default function register(app) {
   });
 
   router.post('/', function *() {
-    const {_id, ...params} = this.request.body;
+    var {_id, ...params} = this.request.body;
     if (_id) {
       // console.log('updating', _id, params);
       yield this.mongo.collection('clients').update(_id.toObjectId(), params);
       this.body = this.request.body;
 
     } else {
+      params.slug = slugify(params.name).toLowerCase();
+
       const inserted = yield this.mongo.collection('clients').insert(params);
       // console.log('inserted', inserted);
       const insertedId = inserted.insertedIds[1];
