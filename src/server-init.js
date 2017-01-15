af4bdc3fe0deaecce950d0a7a4685193d65ac25d
Laoujin <woutervs@hotmail.com>
2017-01-15 19:03:23 +0100

    config: added basePath property

--- src/server-init.js
+++ src/server-init.js
@@ -18,8 +18,8 @@ export const createApp = config => {
   app.use(koaCors());
   app.use(koaBodyParser());
   // app.use(koaServe(__dirname + '../public'));
-  app.use(koaServe('./public'));
-  app.use(koaServe('./templates'));
+  app.use(koaServe(`./${config.basePath}public`));
+  app.use(koaServe(`./${config.basePath}templates`));
 
   const db = config.db;
   app.use(koaMongo({
