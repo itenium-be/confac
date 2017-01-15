7b54661fabab28088d7c24c3a88125a8f60414cc
Laoujin <woutervs@hotmail.com>
2017-01-15 03:59:52 +0100

    config: if there is no record yet, provide some default values

--- resources/config.js
+++ resources/config.js
@@ -6,13 +6,42 @@ export default function register(app) {
   });
 
   router.get('/', function *() {
-    this.body = yield this.mongo.collection('config').findOne({name: 'pongit'});
+    const conf = yield this.mongo.collection('config').findOne({key: 'conf'});
+    if (!conf) {
+      this.body = {
+        key: 'conf',
+        company: {
+          template: 'example-1.pug',
+          name: 'Jouw bedrijfsnaam',
+          address: 'Streetname 15',
+          city: '9000 Brussel',
+          btw: '0000.000.000',
+          bank: 'Banknaam',
+          iban: 'BE00 0000 0000 0000',
+          bic: 'GKCCBEAD',
+          telephone: '0000 / 00 00 00',
+          email: 'ceo@yahoo.com',
+          website: 'www.clicky-me-IT.be'
+        },
+        defaultClient: null,
+      };
+    } else {
+      this.body = conf;
+    }
   });
 
   router.post('/', function *() {
     const {_id, ...params} = this.request.body;
-    yield this.mongo.collection('config').update(_id.toObjectId(), params);
-    this.body = params;
+    if (_id) {
+      yield this.mongo.collection('config').update(_id.toObjectId(), params);
+      this.body = params;
+
+    } else {
+      const inserted = yield this.mongo.collection('config').insert(params);
+      // console.log('inserted', inserted);
+      const insertedId = inserted.insertedIds[1];
+      this.body = Object.assign(params, insertedId.toString().toObjectId());
+    }
   });
 
   app.use(router.routes());
