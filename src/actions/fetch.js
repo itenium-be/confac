cb902994e10715134f134c8d3c7fc852699bf3a4
Laoujin <woutervs@hotmail.com>
2017-01-15 04:25:05 +0100

    moved backend server config to config-front.json

--- src/actions/fetch.js
+++ src/actions/fetch.js
@@ -1,7 +1,7 @@
 import {failure} from './appActions.js';
 import {store} from '../store.js';
 
-const urlPrefix = 'http://localhost:3001/api'; // TODO: put this in some environment config
+const urlPrefix = require('../../config-front.json').backend;
 
 export function buildUrl(url) {
   return urlPrefix + url;
@@ -13,29 +13,3 @@ export function catchHandler(err) {
   console.log('oepsie', err); // eslint-disable-line
   store.dispatch(failure(err));
 }
-
-// export const httpPost = (url, data) => request.post(buildUrl(url))
-//   .set('Content-Type', 'application/json')
-//   .send(data)
-//   .end((err, res) => {
-//     console.log("res", res);
-//     return res.json();
-//   });
-
-// const httpGet = url => {
-//   console.log('httpGet', url);
-//   return request.get(urlPrefix + url)
-//   .set('Accept', 'application/json')
-//   .end(function(err, res) {
-//     console.log('wuuk', url, res);
-//     return JSON.parse(res.body);
-//   });
-// }
-
-// const httpPost = (url, body) => request.post(urlPrefix + url)
-//   .set('Content-Type', 'application/json')
-//   .send(body)
-//   .end(function(err, res) {
-//     console.log('posted return', res);
-//     return res.body;
-//   });
