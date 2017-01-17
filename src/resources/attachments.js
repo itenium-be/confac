6a3aa1a062ad3439cd94d5cff1afd4f760aec7c7
Laoujin <woutervs@hotmail.com>
2017-01-17 03:31:58 +0100

    delete an invoice attachment

--- src/resources/attachments.js
+++ src/resources/attachments.js
@@ -13,6 +13,7 @@ export default function register(app) {
     this.body = attachment[type];
   });
 
+
   router.put('/:invoiceId/:type', body(), function *() {
     const {invoiceId, type} = this.params;
 
@@ -27,21 +28,45 @@ export default function register(app) {
       const {_id, ...newInvoice} = invoice;
 
       // koa-better-body:
-      const {path} = this.request.files[0];
+      const file = this.request.files[0];
       // console.log(this.request.files[0]);
       // console.log(this.request.fields);
 
       // o'rly?
       var fs = require('fs');
-      fileBuffer = fs.readFileSync(path);
+      fileBuffer = fs.readFileSync(file.path);
 
-      newInvoice.attachments.push(type);
+      newInvoice.attachments.push({
+        type,
+        fileName: file.name,
+        fileType: file.type,
+        lastModifiedDate: file.lastModifiedDate
+      });
       yield this.mongo.collection('invoices').update(_id.toString().toObjectId(), newInvoice);
     }
 
     yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: fileBuffer}});
 
     this.body = invoice;
+  });
+
+
+  router.delete('/:invoiceId/:type', body(), function *() {
+    const {invoiceId, type} = this.params;
+
+    var invoice = yield this.mongo.collection('invoices').findOne(invoiceId.toObjectId());
+    if (type === 'pdf') {
+      this.status = 500;
+      this.body = 'cant delete invoice itself';
+      return;
+    }
+
+    const {_id, ...newInvoice} = invoice;
+    newInvoice.attachments = newInvoice.attachments.filter(att => att.type !== type);
+    //console.log('upd', type, newInvoice.attachments);
+    yield this.mongo.collection('invoices').update(_id.toString().toObjectId(), newInvoice);
+    yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: undefined}});
+    this.body = Object.assign(newInvoice, {_id});
   });
 
   app.use(router.routes());
