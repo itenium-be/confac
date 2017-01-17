8174700ddd04c4f93c27c22b43ff62ca2b51e23a
Laoujin <woutervs@hotmail.com>
2017-01-17 05:04:32 +0100

    attachments: also allow client attachments

--- src/resources/attachments.js
+++ src/resources/attachments.js
@@ -7,25 +7,27 @@ export default function register(app) {
     prefix: '/api/attachments'
   });
 
-  router.get('/:invoiceId/:type', function *() {
-    const {invoiceId, type} = this.params;
-    const attachment = yield this.mongo.collection('attachments').findOne(invoiceId.toObjectId(), {[type]: 1});
+  router.get('/:model/:id/:type', function *() {
+    const {id, model, type} = this.params;
+    const coll = model === 'invoice' ? 'attachments' : 'attachments_client';
+    const attachment = yield this.mongo.collection(coll).findOne(id.toObjectId(), {[type]: 1});
     this.body = attachment[type];
   });
 
 
-  router.put('/:invoiceId/:type', body(), function *() {
-    const {invoiceId, type} = this.params;
+  router.put('/:model/:id/:type', body(), function *() {
+    const {id, model, type} = this.params;
 
-    var invoice = yield this.mongo.collection('invoices').findOne(invoiceId.toObjectId());
+    const modelColl = model === 'invoice' ? 'invoices' : 'clients';
+    var modelObj = yield this.mongo.collection(modelColl).findOne(id.toObjectId());
 
     var fileBuffer;
-    if (type === 'pdf') {
-      const html = createHtml(invoice, this.request.origin);
+    if (type === 'pdf' && model === 'invoice') {
+      const html = createHtml(modelObj, this.request.origin);
       fileBuffer = yield htmlToBuffer(html);
 
     } else {
-      const {_id, ...newInvoice} = invoice;
+      const {_id, ...newInvoice} = modelObj;
 
       // koa-better-body:
       const file = this.request.files[0];
@@ -42,31 +44,34 @@ export default function register(app) {
         fileType: file.type,
         lastModifiedDate: file.lastModifiedDate
       });
-      yield this.mongo.collection('invoices').update(_id.toString().toObjectId(), newInvoice);
+      yield this.mongo.collection(modelColl).update(_id.toString().toObjectId(), newInvoice);
     }
 
-    yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: fileBuffer}});
-
-    this.body = invoice;
+    const coll = model === 'invoice' ? 'attachments' : 'attachments_client';
+    yield this.mongo.collection(coll).update(id.toObjectId(), {$set: {[type]: fileBuffer}}, {upsert: true});
+    this.body = modelObj;
   });
 
 
-  router.delete('/:invoiceId/:type', body(), function *() {
-    const {invoiceId, type} = this.params;
+  router.delete('/:model/:id/:type', body(), function *() {
+    const {id, model, type} = this.params;
 
-    var invoice = yield this.mongo.collection('invoices').findOne(invoiceId.toObjectId());
-    if (type === 'pdf') {
+    const modelColl = model === 'invoice' ? 'invoices' : 'clients';
+    var modelObj = yield this.mongo.collection(modelColl).findOne(id.toObjectId());
+    if (type === 'pdf' && model === 'invoice') {
       this.status = 500;
       this.body = 'cant delete invoice itself';
       return;
     }
 
-    const {_id, ...newInvoice} = invoice;
-    newInvoice.attachments = newInvoice.attachments.filter(att => att.type !== type);
-    //console.log('upd', type, newInvoice.attachments);
-    yield this.mongo.collection('invoices').update(_id.toString().toObjectId(), newInvoice);
-    yield this.mongo.collection('attachments').update(invoiceId.toObjectId(), {$set: {[type]: undefined}});
-    this.body = Object.assign(newInvoice, {_id});
+    const {_id, ...newObj} = modelObj;
+    newObj.attachments = newObj.attachments.filter(att => att.type !== type);
+    //console.log('upd', type, newObj.attachments);
+    yield this.mongo.collection(modelColl).update(_id.toString().toObjectId(), newObj);
+
+    const coll = model === 'invoice' ? 'attachments' : 'attachments_client';
+    yield this.mongo.collection(coll).update(id.toObjectId(), {$set: {[type]: undefined}});
+    this.body = Object.assign(newObj, {_id});
   });
 
   app.use(router.routes());
