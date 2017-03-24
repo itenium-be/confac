71d8e8f8a8a8ae246c78c8411d52df8d9a352260
Laoujin <woutervs@hotmail.com>
2017-03-24 03:01:38 +0100

    environment vars

--- config-front.js
+++ config-front.js
@@ -4,5 +4,5 @@ console.log("yaye", process.env);
 if (process.env.REACT_APP_BACKEND_API_URL) {
   module.exports = {backend: process.env.REACT_APP_BACKEND_API_URL, environment: process.env.NODE_ENV || '???'};
 } else {
-  module.exports = {backend: 'http://localhost:3001/api', environment: 'dev'};
+  module.exports = {backend: 'http://localhost:3001/api', environment: process.env.NODE_ENV || 'dev'};
 }
