295db19e4fcf5b687d47d103dc64da9412a9bd14
Laoujin <woutervs@hotmail.com>
2017-03-24 01:40:08 +0100

    todo

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
 export * from './ActionTypes.js';
+
 export * from './appActions.js';
 export * from './initialLoad.js';
 export * from './invoiceActions.js';
 
-// superagent-bluebird-promise bug:
+// TODO: superagent-bluebird-promise bug:
 // https://github.com/KyleAMathews/superagent-bluebird-promise/issues/68
 // Any error on the backend results in a long warning in the console
 // because of missing .stack property on the error
 // It is set in the code with Object.defineProperty but apparently that is not good enough for bluebird
+// (and this is here, why?)
