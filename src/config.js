d4b913c5eba588121176bb60369d8b6cab766481
Laoujin <woutervs@hotmail.com>
2017-03-27 23:36:36 +0200

    jenkins config

--- src/config.js
+++ src/config.js
@@ -1,30 +1,50 @@
-console.log('yaye');
-console.log('yaye', process.env);
+var config;
+if (process.env.REACT_APP_DEPLOY) {
+  switch (process.env.REACT_APP_DEPLOY) {
+  case 'jos':
+    config = {
+      db: {
+        host: 'localhost',
+        db: 'confac',
+        port: 32770
+      },
+      server: {
+        port: 9000,
+        basePath: 'dist/'
+      },
+      env: process.env.NODE_ENV || 'dev'
+    };
+    break;
 
-if (process.env.REACT_APP_DB_HOST) {
-  module.exports = {
-    db: {
-      host: process.env.REACT_APP_DB_HOST,
-      db: process.env.REACT_APP_DB_DB,
-      port: process.env.REACT_APP_DB_PORT
-    },
-    server: {
-      port: process.env.REACT_APP_SERVER_PORT,
-      basePath: process.env.REACT_APP_SERVER_BASE_PATH
-    },
-    env: process.env.NODE_ENV || '???'
-  };
-} else {
-  module.exports = {
-    db: {
-      host: 'localhost',
-      db: 'confac-dev',
-      port: 27018
-    },
-    server: {
-      port: 3001,
-      basePath: ''
-    },
-    env: process.env.NODE_ENV || 'dev'
-  };
+  case 'pongit':
+    config = {
+      db: {
+        host: 'localhost',
+        db: 'confac',
+        port: 32768
+      },
+      server: {
+        port: 9000,
+        basePath: ''
+      },
+      env: process.env.NODE_ENV || 'dev'
+    };
+    break;
+
+  default:
+    config = null;
+  }
 }
+
+module.exports = config || {
+  db: {
+    host: 'localhost',
+    db: 'confac-dev',
+    port: 27018
+  },
+  server: {
+    port: 3001,
+    basePath: ''
+  },
+  env: process.env.NODE_ENV || 'dev'
+};
