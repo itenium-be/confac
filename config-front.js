5e6da7611e37ea68ec6f3176d14fdf15de22e4f2
Laoujin <woutervs@hotmail.com>
2017-03-27 22:46:58 +0200

    jenkins fun

--- config-front.js
+++ config-front.js
@@ -1,8 +1,17 @@
-console.log("yaye");
-console.log("yaye", process.env);
+function getBackend() {
+  switch (process.env.REACT_APP_DEPLOY) {
+  case 'jos':
+    return 'http://192.168.1.4:9000/api';
+  case 'pongit':
+    return 'http://synology-pongit:9000/api';
+  default
+    return 'oepsie!';
+  }
+}
 
-if (process.env.REACT_APP_BACKEND_API_URL) {
-  module.exports = {backend: process.env.REACT_APP_BACKEND_API_URL, environment: process.env.NODE_ENV || '???'};
+if (process.env.REACT_APP_DEPLOY) {
+  const backend = getBackend();
+  module.exports = {backend: backend, environment: process.env.NODE_ENV || '???'};
 } else {
   module.exports = {backend: 'http://localhost:3001/api', environment: process.env.NODE_ENV || 'dev'};
 }
