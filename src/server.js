899387dc4effb471c64968fc4899f6b55b96a08b
Laoujin <woutervs@hotmail.com>
2018-04-11 18:29:37 +0200

    add logging on startup

--- src/server.js
+++ src/server.js
@@ -3,4 +3,6 @@ require('babel-polyfill');
 var createApp = require('./server-init.js').createApp;
 var config = require('./config.js');
 var app = createApp(config);
-app.listen(config.server.port);
+console.log('Starting at ' + config.server.port);
+console.log('Config', JSON.stringify(config));
+app.listen(config.server.port, () => console.log('Started!'));
