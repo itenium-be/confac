54ce6c898d3937bc6a54938e9d31506953c2df9c
Laoujin <woutervs@hotmail.com>
2018-08-16 00:38:24 +0200

    config

--- src/config.js
+++
@@ -1,2 +0,0 @@
-const json = require('./config.json');
-module.exports = Object.assign({env: process.env.NODE_ENV || 'dev'}, json);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,24 @@
 require('babel-polyfill');
 
 var createApp = require('./server-init.js').createApp;
-var config = require('./config.js');
+
+var json = require('./config.json');
+if (process.env.SERVER_HOST) {
+  json = {
+    db: {
+      host: process.env.MONGO_HOST || json.db.host,
+      db: process.env.MONGO_DB || json.db.db,
+      port: process.env.MONGO_PORT || json.db.port,
+    },
+    server: {
+      port: process.env.SERVER_PORT || json.server.port,
+      basePath: process.env.SERVER_BASEPATH || json.server.basePath,
+    }
+  };
+}
+
+var config = Object.assign({env: process.env.NODE_ENV || 'dev'}, json);
+
 var app = createApp(config);
 console.log('Starting at ' + config.server.port);
 console.log('Config', JSON.stringify(config));
