825539fb1905094234544e3fbab6aee166aef2c3
Laoujin <woutervs@hotmail.com>
2019-08-31 17:34:32 +0200

    Quotations<->Invoices link. Easier filters for quotations.

--- src/components/invoice/invoice-list/QuotationList.tsx
+++ src/components/invoice/invoice-list/QuotationList.tsx
@@ -1,28 +1,65 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Container} from 'react-bootstrap';
-
-import * as Control from '../../controls';
 import {updateInvoiceFilters} from '../../../actions/index';
-import {InvoiceList} from './InvoiceList';
-import {t} from '../../util';
+import InvoiceListModel from '../models/InvoiceListModel';
+import {Container, Row, Col} from 'react-bootstrap';
+import {GroupedInvoiceTable} from '../invoice-table/GroupedInvoiceTable';
+import {NonGroupedInvoiceTable} from '../invoice-table/NonGroupedInvoiceTable';
 import { ConfacState } from '../../../reducers/default-states';
+import { ConfigModel } from '../../config/models/ConfigModel';
+import InvoiceModel from '../models/InvoiceModel';
+import { ClientModel } from '../../client/models/ClientModels';
+import { InvoiceFilters } from '../../../models';
+import { t } from '../../util';
+import { QuotationSearch } from './QuotationSearch';
+import { Link } from 'react-router-dom';
+import { Icon } from '../../controls';
 
 
-export default class QuotationList extends Component {
+type QuotationListProps = {
+  config: ConfigModel,
+  invoices: InvoiceModel[],
+  clients: ClientModel[],
+  updateInvoiceFilters: any,
+  filters: InvoiceFilters,
+}
+
+export class QuotationList extends Component<QuotationListProps> {
   render() {
+    if (!this.props.filters) {
+      return null;
+    }
+
+    const vm = new InvoiceListModel(this.props.invoices, this.props.clients, this.props.filters, true);
+
+    const TableComponent = this.props.filters.groupedByMonth ? GroupedInvoiceTable : NonGroupedInvoiceTable;
     return (
       <Container className="quotation-list">
-        <Control.AddIcon onClick="/quotations/create" label={t('quotation.createNew')} data-tst="new-quotation" />
-        <ConnectedQuotationList />
+        <Row>
+          <Col xs={8}>
+              <h1>{t('quotation.title')}</h1>
+            </Col>
+            <Col xs={4} style={{textAlign: 'right'}}>
+              <Link to="/invoices" className="btn btn-light">
+                {t('title')}
+                <Icon fa="fa fa-arrow-right" size={1} style={{marginLeft: 8}} />
+              </Link>
+            </Col>
+        </Row>
+        <QuotationSearch
+          onChange={(newFilter: InvoiceFilters) => this.props.updateInvoiceFilters(newFilter)}
+          filterOptions={vm.getFilterOptions()}
+          filters={this.props.filters}
+        />
+        <TableComponent vm={vm} config={this.props.config} />
       </Container>
     );
   }
 }
 
-const ConnectedQuotationList = connect((state: ConfacState) => ({
+export default connect((state: ConfacState) => ({
   invoices: state.invoices.filter(x => x.isQuotation),
   clients: state.clients,
   filters: state.app.invoiceFilters,
   config: state.config,
-}), {updateInvoiceFilters})(InvoiceList);
+}), {updateInvoiceFilters})(QuotationList);
---
+++ src/components/invoice/invoice-list/QuotationSearch.tsx
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Row, Col} from 'react-bootstrap';
+import { InvoiceFiltersSearch, InvoiceFilters } from '../../../models';
+import { SearchStringInput } from '../../controls/form-controls/inputs/SearchStringInput';
+import { InvoiceSearchSelect } from '../controls/InvoiceSearchSelect';
+
+
+type QuotationSearchProps = {
+  filterOptions: InvoiceFiltersSearch[],
+  onChange: (newFilter: InvoiceFilters) => void,
+  filters: InvoiceFilters,
+}
+
+
+export class QuotationSearch extends Component<QuotationSearchProps> {
+  onFilterChange(updateObj: any) {
+    const newFilter: InvoiceFilters = Object.assign({}, this.props.filters, updateObj);
+    this.props.onChange(newFilter);
+  }
+
+  render() {
+    const {search, freeInvoice} = this.props.filters;
+    return (
+      <Row>
+        <Col sm={6}>
+          <SearchStringInput
+            value={freeInvoice}
+            onChange={str => this.onFilterChange({freeInvoice: str})}
+          />
+        </Col>
+        <Col sm={6}>
+          <InvoiceSearchSelect
+            onChange={(value: InvoiceFiltersSearch[]) => this.onFilterChange({search: value})}
+            value={search}
+            options={this.props.filterOptions}
+            data-tst="filter-all"
+          />
+        </Col>
+      </Row>
+    );
+  }
+}
