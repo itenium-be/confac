3d125506921c8f24583bb4026faef8fd2f8af5ed
Laoujin <woutervs@hotmail.com>
2019-09-02 20:36:57 +0200

    default backend port: 9000

--- src/config-front.js
+++ src/config-front.js
@@ -4,5 +4,5 @@ if (process.env.NODE_ENV === 'production') {
   const backend = window.location.origin + '/api';
   module.exports = {backend: backend};
 } else {
-  module.exports = {backend: 'http://localhost:3001/api'};
+  module.exports = {backend: 'http://localhost:9000/api'};
 }
