a24b2a628275cbcd3345c826e8a4436793eb63f7
Laoujin <woutervs@hotmail.com>
2019-09-17 00:35:01 +0200

    do not crash with backend db that does not include email etcé

--- src/reducers/config-reducers.ts
+++ src/reducers/config-reducers.ts
@@ -8,7 +8,7 @@ export const config = (state: ConfigModel = defaultConfig, action): ConfigModel 
   switch (action.type) {
   case ACTION_TYPES.CONFIG_FETCHED:
     console.log('CONFIG_FETCHED', action.config); // eslint-disable-line
-    return action.config;
+    return {...defaultConfig, ...action.config};
 
   case ACTION_TYPES.CONFIG_UPDATE:
     return action.config;
