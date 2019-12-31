d4f93b7fb9014e154e9f0b8fcb90f381a94d20f4
Jawad Sefiani <jawad.sefiani@kapsch.net>
2019-12-31 17:47:08 +0100

    feature: route for creating consultant

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import {AppWithLayout} from './components/AppWithLayout';
+import { AppWithLayout } from './components/AppWithLayout';
 import EditInvoice from './components/invoice/invoice-edit/EditInvoice';
 import InvoiceList from './components/invoice/invoice-list/InvoiceList';
 import QuotationList from './components/invoice/invoice-list/QuotationList';
 import ClientList from './components/client/ClientList';
 import EditClient from './components/client/EditClient';
 import EditConfig from './components/config/EditConfig';
+import EditConsultant from './components/consultant/EditConsultant'
 
 const Routes = () => (
   <BrowserRouter>
@@ -23,6 +24,8 @@ const Routes = () => (
       <Route path="/clients/create" render={props => <AppWithLayout Component={EditClient} {...props} />} />
       <Route path="/clients/:id" render={props => <AppWithLayout Component={EditClient} {...props} />} />
       <Route path="/clients" render={props => <AppWithLayout Component={ClientList} {...props} />} />
+
+      <Route path="/consultants/create" render={props => <AppWithLayout Component={EditConsultant} {...props} />} />
 
       <Route path="/config" render={props => <AppWithLayout Component={EditConfig} {...props} />} />
 
