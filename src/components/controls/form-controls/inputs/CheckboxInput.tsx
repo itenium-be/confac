c385b501b61cc9ac529e995b5e148bd12b1d9e34
Wouter Van Schandevijl <woutervs@hotmail.com>
2019-12-31 16:31:43 +0100

    CheckboxInput: styling & onChange

--- src/components/controls/form-controls/inputs/CheckboxInput.tsx
+++ src/components/controls/form-controls/inputs/CheckboxInput.tsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import {FormCheck} from 'react-bootstrap';
-import {EnhanceInputWithLabel, EnhanceInputWithLabelProps} from '../../../enhancers/EnhanceInputWithLabel';
+import {/*EnhanceInputWithLabel, */EnhanceInputWithLabelProps} from '../../../enhancers/EnhanceInputWithLabel';
 
 export type BaseInputProps<TReturn> = EnhanceInputWithLabelProps & {
   value?: boolean,
@@ -8,15 +7,19 @@ export type BaseInputProps<TReturn> = EnhanceInputWithLabelProps & {
   style?: React.CSSProperties,
 }
 
-export const CheckboxInput = EnhanceInputWithLabel(class extends Component<BaseInputProps<any>> {
+export const CheckboxInput = /*EnhanceInputWithLabel(*/class extends Component<BaseInputProps<any>> {
   render() {
-    const {value, ...props} = this.props;
+    const {value, label, onChange, ...props} = this.props;
     return (
-      <FormCheck
-        checked={value}
-        inline
-        {...props}
-      />
+      <div style={{marginBottom: 5}}>
+        <input
+          type="checkbox"
+          checked={value || false}
+          onChange={evt => onChange(evt.target.checked)}
+          {...props}
+        />
+        <span style={{paddingLeft: 10}}>{label}</span>
+      </div>
     );
   }
-});
+}/*)*/;
