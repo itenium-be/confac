7fe4a610b27f7ff0ceda345a8276cbbbdb96f617
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-02 20:12:20 +0100

    feature: extended interface with new '_id' property

--- src/components/consultant/models/ConsultantModel.ts
+++ src/components/consultant/models/ConsultantModel.ts
@@ -1,4 +1,9 @@
 export interface ConsultantModel {
+  /**
+    For now we can only create a consultant 
+    and not edit an existing one that's why we make the '_id' property optional.
+  */
+  _id?: string,
   name: string,
   firstName: string,
   type: string,
