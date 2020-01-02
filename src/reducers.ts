8e5461b4ff11779b392f5a058e135ae513afd2c4
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-02 20:05:26 +0100

    feature: projects reducers

--- src/reducers.ts
+++ src/reducers.ts
@@ -3,3 +3,4 @@ export * from './reducers/client-reducers';
 export * from './reducers/config-reducers';
 export * from './reducers/invoice-reducers';
 export * from './reducers/consultant-reducers'
+export * from './reducers/project-reducers'
---
+++ src/reducers/project-reducers.ts
@@ -0,0 +1,14 @@
+import { ACTION_TYPES } from "../actions";
+import { ProjectModel } from "../components/project/models";
+
+export const projects = (state: ProjectModel[] = [], action): ProjectModel[] => {
+  switch (action.type) {
+    case ACTION_TYPES.PROJECTS_FETCHED:
+      return action.projects
+    case ACTION_TYPES.PROJECT_UPDATE:
+      let newState: ProjectModel[] = [...state, action.project]
+      return newState;
+    default:
+      return state
+  }
+}
