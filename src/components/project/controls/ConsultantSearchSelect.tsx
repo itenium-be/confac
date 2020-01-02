2e84960c476636bd68910fba50b81dc6f8612f36
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-02 20:17:20 +0100

    feature: search and select consultant component

---
+++ src/components/project/controls/ConsultantSearchSelect.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { t } from '../../util';
+import { EnhanceInputWithLabel } from '../../enhancers/EnhanceInputWithLabel';
+import Select from 'react-select';
+import { ConfacState } from '../../../reducers/app-state';
+import { ConsultantModel } from '../../consultant/models';
+import { SelectItem } from '../../../models';
+
+
+type ConsultantSelectProps = {
+  consultants: ConsultantModel[],
+  /**
+   * The client _id
+   */
+  value: string,
+  onChange: (clientId: string, consultant: ConsultantModel) => void,
+}
+
+const _ConsultantSearchSelect = (props: ConsultantSelectProps) => {
+  const { value, consultants, ...rest } = props;
+
+  const getConsultant = (consultantId: string): ConsultantModel => {
+    return props.consultants.find(c => c._id === consultantId) as ConsultantModel;
+  }
+
+  const selectedConsultantId = value && typeof value === 'object' ? value['_id'] : value;
+
+  const options: SelectItem[] = consultants
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .map(item => {
+      return { value: item._id, label: item.name } as SelectItem
+    });
+
+  const selectedOption = options.find(o => o.value === selectedConsultantId);
+
+  return (
+    <Select
+      value={selectedOption}
+      options={options as any}
+      onChange={((item: SelectItem) => props.onChange(item && item.value as string, item && getConsultant(item.value as string))) as any}
+      isClearable={true}
+      placeholder={t('controls.selectPlaceholder')}
+      className="tst-client-select"
+    />
+  );
+}
+
+export const ConsultantSearchSelect = EnhanceInputWithLabel(connect((state: ConfacState) => ({ consultants: state.consultants }))(_ConsultantSearchSelect));
