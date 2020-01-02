fcfca384eb95c9a6d0315bc968bd3ea343738c43
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-02 20:02:40 +0100

    styling: modified box-shadow values and created page title container class

--- src/components/controls/skeleton/StickyFooter.tsx
+++ src/components/controls/skeleton/StickyFooter.tsx
@@ -1,11 +1,13 @@
 import { Navbar, Nav } from "react-bootstrap";
 import React from "react";
 
-export const StickyFooter = ({children}) => {
+export const StickyFooter = ({ children }) => {
   return (
     <Navbar fixed="bottom" className="edit-footer">
       <Nav.Item className="navbar-center button-row">
-        {children}
+        <div className="footer-container">
+          {children}
+        </div>
       </Nav.Item>
     </Navbar>
   );
