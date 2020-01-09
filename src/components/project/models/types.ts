afdad47363b3a08d49ca98ab44b228a0be3c6094
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-10 00:44:42 +0100

    feature: created ProjectMonth model

--- src/components/project/models/types.ts
+++ src/components/project/models/types.ts
@@ -1,24 +1,31 @@
-import { ConsultantModel } from "../../consultant/models";
-import { ClientModel } from "../../client/models/ClientModels";
+import {Moment} from 'moment';
+import {ConsultantModel} from '../../consultant/models';
+import {ClientModel} from '../../client/models/ClientModels';
 
 export interface ProjectModel {
-  _id?: string,
-  consultantId: string,
-  startDate: string,
-  endDate: string,
-  partner: string,
-  partnerTariff: number,
-  client: string,
-  clientTariff: number,
+  _id?: string;
+  consultantId: string;
+  startDate: string;
+  endDate: string;
+  partner: string;
+  partnerTariff: number;
+  client: string;
+  clientTariff: number;
+}
+
+export interface ProjectMonthModel {
+  _id?: string;
+  month: Moment;
+  projectId?: string;
 }
 
 export interface FullProjectModel {
-  details: ProjectDetailsModel,
-  consultant: ConsultantModel,
-  client: ClientModel,
-  partner: ClientModel
+  details: ProjectDetailsModel;
+  consultant: ConsultantModel;
+  client: ClientModel;
+  partner: ClientModel;
 }
 
 export interface ProjectDetailsModel extends ProjectModel {
-  isActive: boolean
+  isActive: boolean;
 }
