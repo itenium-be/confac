528448cb30b6daf7189f3bea1e23bb2bda260c43
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-10 00:38:57 +0100

    feature: get all project months on initial load

--- src/actions/initialLoad.ts
+++ src/actions/initialLoad.ts
@@ -1,100 +1,112 @@
-import { ACTION_TYPES } from './utils/ActionTypes';
-import { buildUrl } from './utils/fetch';
-import { failure } from './appActions';
-import { toast } from 'react-toastify';
+import {toast} from 'react-toastify';
+import {ACTION_TYPES} from './utils/ActionTypes';
+import {buildUrl} from './utils/fetch';
+import {failure} from './appActions';
 
 let counter: number;
 
-const httpGet = (url: string) => fetch(buildUrl(url))
-  .then(res => res.json(), err => {
-    console.log('Initial Load Failure', err);
-    if (counter === 0) {
-      failure(err.message, 'Initial Load Failure', undefined, toast.POSITION.BOTTOM_RIGHT as any);
-    }
-    counter++;
-    return Promise.reject(err);
-  })
-  .then(data => {
-    if (data.message && data.stack) {
-      console.log('Initial Load Failure', data);
-      if (counter === 0) {
-        failure(data.message, 'Initial Load Failure', undefined, toast.POSITION.BOTTOM_RIGHT as any);
+const httpGet = (url: string) =>
+  fetch(buildUrl(url))
+    .then(
+      res => res.json(),
+      err => {
+        console.log('Initial Load Failure', err);
+        if (counter === 0) {
+          failure(err.message, 'Initial Load Failure', undefined, toast.POSITION.BOTTOM_RIGHT as any);
+        }
+        counter++;
+        return Promise.reject(err);
+      },
+    )
+    .then(data => {
+      if (data.message && data.stack) {
+        console.log('Initial Load Failure', data);
+        if (counter === 0) {
+          failure(data.message, 'Initial Load Failure', undefined, toast.POSITION.BOTTOM_RIGHT as any);
+        }
+        counter++;
+        return Promise.reject(data);
       }
-      counter++;
-      return Promise.reject(data);
-    }
-    return data;
-  });
+      return data;
+    });
 
 function fetchClients() {
   return dispatch => {
-    return httpGet('/clients')
-      .then(data => {
-        dispatch({
-          type: ACTION_TYPES.CLIENTS_FETCHED,
-          clients: data
-        });
+    return httpGet('/clients').then(data => {
+      dispatch({
+        type: ACTION_TYPES.CLIENTS_FETCHED,
+        clients: data,
       });
+    });
   };
 }
 
 function fetchConsultants() {
   return dispatch => {
-    return httpGet('/consultants')
-      .then(data => {
-        dispatch({
-          type: ACTION_TYPES.CONSULTANTS_FETCHED,
-          consultants: data
-        })
-      })
-  }
+    return httpGet('/consultants').then(data => {
+      dispatch({
+        type: ACTION_TYPES.CONSULTANTS_FETCHED,
+        consultants: data,
+      });
+    });
+  };
 }
 
 function fetchProjects() {
   return dispatch => {
-    return httpGet('/projects')
-      .then(data => {
-        dispatch({
-          type: ACTION_TYPES.PROJECTS_FETCHED,
-          projects: data
-        })
-      })
-  }
+    return httpGet('/projects').then(data => {
+      dispatch({
+        type: ACTION_TYPES.PROJECTS_FETCHED,
+        projects: data,
+      });
+    });
+  };
 }
 
 function fetchConfig() {
   return dispatch => {
-    return httpGet('/config')
-      .then(data => {
-        dispatch({
-          type: ACTION_TYPES.CONFIG_FETCHED,
-          config: data
-        });
+    return httpGet('/config').then(data => {
+      dispatch({
+        type: ACTION_TYPES.CONFIG_FETCHED,
+        config: data,
       });
+    });
   };
 }
 
 function fetchInvoices() {
   return dispatch => {
-    return httpGet('/invoices')
-      .then(data => {
-        dispatch({
-          type: ACTION_TYPES.INVOICES_FETCHED,
-          invoices: data
-        });
+    return httpGet('/invoices').then(data => {
+      dispatch({
+        type: ACTION_TYPES.INVOICES_FETCHED,
+        invoices: data,
       });
+    });
+  };
+}
+
+function fetchProjectsMonth() {
+  return dispatch => {
+    return httpGet('/projects/month').then(data => {
+      dispatch({
+        type: ACTION_TYPES.PROJECTS_MONTH_FETCHED,
+        projectsMonth: data,
+      });
+    });
   };
 }
 
 export function initialLoad(): any {
   counter = 0;
-  return dispatch => Promise.all([
-    dispatch(fetchClients()),
-    dispatch(fetchConfig()),
-    dispatch(fetchInvoices()),
-    dispatch(fetchConsultants()),
-    dispatch(fetchProjects())
-  ]).then(() => {
-    dispatch({ type: ACTION_TYPES.INITIAL_LOAD });
-  });
+  return dispatch =>
+    Promise.all([
+      dispatch(fetchClients()),
+      dispatch(fetchConfig()),
+      dispatch(fetchInvoices()),
+      dispatch(fetchConsultants()),
+      dispatch(fetchProjects()),
+      dispatch(fetchProjectsMonth()),
+    ]).then(() => {
+      dispatch({type: ACTION_TYPES.INITIAL_LOAD});
+    });
 }
