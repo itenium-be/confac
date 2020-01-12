6b157e46dbf059e06c71d687431c97cf6e9637c5
Laoujin <woutervs@hotmail.com>
2020-01-12 15:16:25 +0100

    updated linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,21 +18,27 @@ module.exports = {
     sourceType: "module"
   },
   ignorePatterns: ["node_modules", "public", ".vscode"],
-  plugins: ["react", "react-hooks", "@typescript-eslint"],
+  plugins: ["react", "react-hooks", "@typescript-eslint"/*, "typescript"*/],
   rules: {
-    "implicit-arrow-linebreak": ["error", "below"],
-    "no-unused-vars": [1],
+    "implicit-arrow-linebreak": ["error", "beside"],
+    "no-unused-vars": [0, { args: 'none' }], // https://github.com/typescript-eslint/typescript-eslint/issues/46
+    // "no-use-before-define": "warn",
     "react/jsx-filename-extension": [1, { extensions: [".tsx"] }],
     "no-underscore-dangle": [0],
     "object-curly-spacing": [2, "never"],
     "import/extensions": [0],
-    "react/destructuring-assignment": [1],
+    "react/destructuring-assignment": [0],
     "import/prefer-default-export": [0],
     "no-shadow": [1],
-    "no-multiple-empty-lines": ["error", { max: 3 }],
+    "no-multiple-empty-lines": ["error", { max: 7 }],
     "linebreak-style": [0],
-    "max-len": [1],
-    "dot-notation": [1]
+    "max-len": ["error", { "code": 140 }],
+    "dot-notation": [1],
+    "padded-blocks": [0],
+    "object-curly-newline": ["error", { "ImportDeclaration": "never" }],
+    "no-plusplus": [0],
+    "react/no-children-prop": [0],
+    "class-methods-use-this": [0],
   },
   "settings": {
     "import/resolver": {
