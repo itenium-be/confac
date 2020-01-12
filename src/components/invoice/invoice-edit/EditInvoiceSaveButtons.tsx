519c72755865b559b019e3f51f29bb98c92a6c1e
Laoujin <woutervs@hotmail.com>
2020-01-12 22:20:45 +0100

    final linting?

--- src/components/invoice/invoice-edit/EditInvoiceSaveButtons.tsx
+++ src/components/invoice/invoice-edit/EditInvoiceSaveButtons.tsx
@@ -29,10 +29,10 @@ export const EditInvoiceSaveButtonsComponent = ({history, invoice, onClick}: Edi
   const tp = (transKey: string): string => t(invoice.getType() + transKey);
   return (
     <>
-      <BusyButton variant="light" onClick={onClick.bind(null, 'preview', history)} data-tst="preview" icon="far fa-eye">
+      <BusyButton variant="light" onClick={() => onClick('preview', history)} data-tst="preview" icon="far fa-eye">
         {t('invoice.preview')}
       </BusyButton>
-      <BusyButton onClick={onClick.bind(null, isNewInvoice ? 'create' : 'update', history)} data-tst="save">
+      <BusyButton onClick={() => onClick(isNewInvoice ? 'create' : 'update', history)} data-tst="save">
         {isNewInvoice ? tp('.create') : t('save')}
       </BusyButton>
     </>
