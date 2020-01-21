d3243728432de5cec5cef6fd8c4c09618de9b77f
Laoujin <woutervs@hotmail.com>
2020-01-21 20:19:25 +0100

    Don't add TODOs without TODO keyword (so alll TODOs can be found easily from an IDE.) If a TODO can be avoided altogether, even better! In this case we can assign some random default value during initialization. When the 'EditConsultant' is implemented it will no longer be necessary to make (possible extra) changes required by the TODO.

--- src/components/consultant/models/ConsultantModel.ts
+++ src/components/consultant/models/ConsultantModel.ts
@@ -1,9 +1,5 @@
 export interface ConsultantModel {
-  /**
-    For now we can only create a consultant
-    and not edit an existing one that's why we make the '_id' property optional.
-  */
-  _id?: string,
+  _id: string,
   name: string,
   firstName: string,
   type: string,
