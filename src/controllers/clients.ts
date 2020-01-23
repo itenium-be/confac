e68cec7b13a1407037ac829e6f64db9c6276638c
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-01-23 20:24:37 +0100

    feature: route for creating and editing client

--- src/controllers/clients.ts
+++ src/controllers/clients.ts
@@ -1,7 +1,8 @@
 import {Request, Response} from 'express';
+import slugify from 'slugify';
 import fetch from 'node-fetch';
 
-import {ClientsCollection} from '../models/clients';
+import {ClientsCollection, IClient} from '../models/clients';
 
 export const getClients = async (req: Request, res: Response) => {
   const clients = await ClientsCollection.find();
@@ -12,4 +13,25 @@ export const validateBtw = async (req: Request, res: Response) => {
   const url = `https://controleerbtwnummer.eu/api/validate/${req.query.btw}.json`;
   const result = await fetch(url).then(response => response.json());
   return res.send(result);
+};
+
+const updateClient = async (id: string, client: IClient): Promise<IClient> => {
+  const updatedClient = await ClientsCollection.findByIdAndUpdate({_id: id}, client, {new: true});
+  return updatedClient as IClient;
+};
+
+const createClient = async (client: IClient): Promise<IClient> => {
+  const slug = slugify(client.name).toLowerCase();
+  const createdClient = await ClientsCollection.create({...client, slug});
+  return createdClient;
+};
+
+export const createOrUpdateClient = async (req: Request, res: Response) => {
+  const {_id, ...client} = req.body;
+  if (_id) {
+    const updatedClient = await updateClient(_id, client);
+    return res.send(updatedClient);
+  }
+  const createdClient = await createClient(client);
+  return res.send(createdClient);
 };
--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import {getClients, validateBtw} from '../controllers/clients';
+import {getClients, validateBtw, createOrUpdateClient} from '../controllers/clients';
 
 const clientsRouter = Router();
 
 clientsRouter.get('/', getClients);
 clientsRouter.get('/btw/:btw', validateBtw);
+
+clientsRouter.post('/', createOrUpdateClient);
 
 export default clientsRouter;
