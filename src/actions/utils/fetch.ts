6dedd0ba1f83570e2c83ec56ba4c4c606a0073a1
Laoujin <woutervs@hotmail.com>
2020-02-07 21:56:26 +0100

    made fetch error responses more robust

--- src/actions/utils/fetch.ts
+++ src/actions/utils/fetch.ts
@@ -12,13 +12,19 @@ export function buildUrl(url: string): string {
 export function catchHandler(err) {
   console.log('oepsie', err); // eslint-disable-line
   if (err.res.badRequest) {
-    console.log('BadRequest', err.body); // eslint-disable-line
+    if (err.body) {
+      console.error('BadRequest', err.body); // eslint-disable-line
 
-    const msg = t(err.body.msg, err.body.data);
-    failure(msg, 'BadRequest', 5000);
+      const msg = t(err.body.msg, err.body.data);
+      failure(msg, 'BadRequest', 5000);
 
-    if (err.body.reload) {
-      store.dispatch(initialLoad());
+      if (err.body.reload) {
+        store.dispatch(initialLoad());
+      }
+
+    } else {
+      console.error('BadRequest', err.res.error); // eslint-disable-line
+      failure('Unknown Erreur', 'BadRequest', 5000);
     }
     return;
   }
