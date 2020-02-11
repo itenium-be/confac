5944fe55fb61ad0e1213497bddc40a466c773781
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-02-12 00:32:29 +0100

    fix: findActiveProjectsForSelectedMonth not passing

--- src/controllers/tests/projects.test.ts
+++ src/controllers/tests/projects.test.ts
@@ -1,14 +1,19 @@
 /* eslint-disable max-len */
+import {ObjectID} from 'mongodb';
 import {findActiveProjectsForSelectedMonth} from '../projects';
 import {IProject} from '../../models/projects';
 
 const initDate = (date: string) => new Date(date).toISOString();
 
 const createProject = (start: string, end?: string): IProject => ({
-  _id: '1',
+  _id: new ObjectID(),
   consultantId: 'c1',
   startDate: initDate(start),
   endDate: end && initDate(end),
+  projectMonthConfig: {
+    timesheetCheck: false,
+    inboundInvoice: false,
+  },
   client: {
     clientId: 'client1',
     rateType: 'daily',
