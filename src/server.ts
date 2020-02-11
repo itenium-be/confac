64cb722cbc963c68affc44add968e49f58952697
Jawad Sefiani <jawad.sefiani@kapsch.net>
2020-02-12 00:38:25 +0100

    feature: initialize MongoDB + serve static files

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
 import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import sgMail from '@sendgrid/mail';
 import errorHandler from 'errorhandler'; // TODO: This is probably going to crash on the CI?
+import {MongoClient} from 'mongodb';
 import cors from 'cors';
 
 import appConfig from './config';
@@ -13,27 +13,39 @@ const app = express();
 
 sgMail.setApiKey(appConfig.SENDGRID_API_KEY);
 
-if (appConfig.environment === 'development') {
+if (appConfig.ENVIRONMENT === 'development') {
   app.use(errorHandler());
 }
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`./${appConfig.server.basePath}public`));
+
+if (appConfig.ENABLE_ROOT_TEMPLATES) {
+  app.use(express.static('/templates'));
+} else {
+  app.use(express.static(`./${appConfig.server.basePath}templates`));
+}
+
+const connectionString = `mongodb://${appConfig.db.host}:${appConfig.db.port}/${appConfig.db.db}`;
+let _MongoClient: MongoClient;
+MongoClient.connect(connectionString, {useUnifiedTopology: true}).then(client => {
+  console.log('Successfully connected to the database!');
+  _MongoClient = client;
+})
+  .catch(err => console.log(`Could not connect to the database. More info: ${err}`));
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.db = _MongoClient.db();
+  next();
+});
 
 app.use('/api', appRouter);
 
-const connectionString = `mongodb://${appConfig.db.host}:${appConfig.db.port}/${appConfig.db.db}`;
-
-mongoose.connect(connectionString, {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-})
-  .then(() => console.log('Successfully connected to the database!'))
-  .catch(err => console.log(`Could not connect to the database. More info: ${err}`));
+app.use((req: Request, res: Response) => res.status(404).send('Page not found!'));
 
 app.listen(appConfig.server.port, () => {
-  console.log(`Server connected to port ${appConfig.server.port}, running in a ${appConfig.environment} environment.`);
+  console.log(`Server connected to port ${appConfig.server.port}, running in a ${appConfig.ENVIRONMENT} environment.`);
   // console.log(JSON.stringify(config))
 });
