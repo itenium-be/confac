be703db22d06ffbabfba7cfaab8c3f58b93b3b98
Laoujin <woutervs@hotmail.com>
2020-05-18 23:51:58 +0200

    deleted unneccessary branch

--- src/controllers/attachments.ts
+++ src/controllers/attachments.ts
@@ -21,17 +21,8 @@ const saveAttachment = async (req: Request, attachmentModelConfig: IAttachmentMo
     lastModifiedDate: new Date().toISOString(),
   });
 
-
-  let inserted;
-  if (standardCollectionName === CollectionNames.PROJECTS_MONTH && type === 'inbound') {
-    inserted = await req.db.collection(standardCollectionName)
-      .findOneAndUpdate({_id: new ObjectID(_id)}, {
-        $set: {attachments: updatedAttachments},
-      }, {returnOriginal: false});
-  } else {
-    inserted = await req.db.collection(standardCollectionName)
-      .findOneAndUpdate({_id: new ObjectID(_id)}, {$set: {attachments: updatedAttachments}}, {returnOriginal: false});
-  }
+  const inserted = await req.db.collection(standardCollectionName)
+    .findOneAndUpdate({_id: new ObjectID(_id)}, {$set: {attachments: updatedAttachments}}, {returnOriginal: false});
 
   const result = inserted.value;
 
