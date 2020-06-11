c0c4080bcce3123277b47b60ee935d4ef3ed52fe
Laoujin <woutervs@hotmail.com>
2020-06-11 17:17:08 +0200

    bugfix BadRequest: do not show verified=forced records

--- src/components/project/controls/ProjectMonthSelect.tsx
+++ src/components/project/controls/ProjectMonthSelect.tsx
@@ -37,6 +37,7 @@ const ProjectMonthSelectComponent = (props: ProjectMonthSelectProps) => {
 
   const options: SelectItem[] = fullProjectMonths
     .filter(fpm => !fpm.invoice || (props.invoice && props.invoice._id === fpm.invoice._id))
+    .filter(fpm => fpm.details.verified !== 'forced')
     .sort((a, b) => getProjectMonthDesc(a).localeCompare(getProjectMonthDesc(b)))
     .map(item => ({value: item._id, label: getProjectMonthDesc(item)}));
 
