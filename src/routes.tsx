e42cfc07ac72e57cb95577bb62fa40674a1f01c0
Laoujin <woutervs@hotmail.com>
2020-06-24 01:16:26 +0200

    invoicelist as default page

--- src/routes.tsx
+++ src/routes.tsx
@@ -55,7 +55,7 @@ const Routes = () => (
       <Route path="/roles/:id" render={props => <AppWithLayout Component={EditRole} {...props} />} />
       <Route path="/login" render={props => <UnauthicatedAppLayout Component={LoginPage} {...props} />} />
 
-      <Route path="/" render={props => <AppWithLayout Component={ProjectMonthsLists} {...props} />} />
+      <Route path="/" render={props => <AppWithLayout Component={InvoiceList} {...props} />} />
     </Switch>
   </BrowserRouter>
 );
