8c63d09dff85e41c80cee7c6c8aba5477c19da3a
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-01-29 16:49:22 +0100

    create projectMonths: allow manual deselection of projects

--- src/components/project/controls/CreateProjectsMonthModal.tsx
+++ src/components/project/controls/CreateProjectsMonthModal.tsx
@@ -12,8 +12,8 @@ import {ConfacState} from '../../../reducers/app-state';
 import {useProjects} from '../../hooks/useProjects';
 import {FullProjectModel} from '../models/FullProjectModel';
 import {ProjectDurationSmall} from './ProjectDuration';
-import {Link} from '../../controls/Link';
 import {MonthPicker} from '../../controls/form-controls/MonthPicker';
+import {CheckboxInput} from '../../controls/form-controls/inputs/CheckboxInput';
 
 const toFirstDayOfMonth = (value: Moment): Moment => {
   const localMoment = moment(value).startOf('month').startOf('day');
@@ -29,7 +29,7 @@ function useToBeCreatedProjects(month: Moment): FullProjectModel[] {
   const existingProjectIds = existingProjectsMonth.map(x => x.projectId);
   const activeProjects = useProjects(month).filter(x => x.isActiveInMonth(month));
   const newProjects = activeProjects.filter(prj => !existingProjectIds.includes(prj._id));
-  return newProjects;
+  return newProjects.sort((a, b) => a.consultantName.localeCompare(b.consultantName));
 }
 
 
@@ -57,13 +57,27 @@ export const CreateProjectsMonthModal = (props: ProjectMonthModalProps) => {
   const defaultNewMonth = toFirstDayOfMonth(useDefaultNewProjectMonth());
   const [date, setDate] = useState<moment.Moment>(defaultNewMonth);
   const newProjects = useToBeCreatedProjects(date);
+  const initialSelection = newProjects.map(() => true);
+  const [forceReload, setForceReload] = useState(false);
+  const [toBeCreated, setToBeCreated] = useState(initialSelection);
+
+  if (forceReload) {
+    setForceReload(false);
+    setToBeCreated(initialSelection);
+  }
+
+  const data = toBeCreated.map((selected, idx) => ({selected, prj: newProjects[idx]}));
+
+  const getSelectedProjects = (): string[] => {
+    return data.filter(p => p.selected).map(p => p.prj._id);
+  }
 
   return (
     <Modal
       show={props.show}
       onClose={props.onClose}
       title={t('projectMonth.createProjects.title')}
-      onConfirm={() => dispatch(createProjectsMonth(date, newProjects.map(x => x._id)) as any)}
+      onConfirm={() => dispatch(createProjectsMonth(date, getSelectedProjects()) as any)}
     >
       <Form>
         <Container>
@@ -71,10 +85,10 @@ export const CreateProjectsMonthModal = (props: ProjectMonthModalProps) => {
             <MonthPicker
               label={t('projectMonth.createProjects.selectMonth')}
               value={date}
-              onChange={value => value && setDate(value)}
+              onChange={value => {value && setDate(value); value && setForceReload(true)}}
             />
           </Row>
-          {newProjects && <ToBeCreated projects={newProjects} />}
+          {newProjects && <ToBeCreated projects={data} setToBeCreated={setToBeCreated} />}
         </Container>
       </Form>
     </Modal>
@@ -82,10 +96,11 @@ export const CreateProjectsMonthModal = (props: ProjectMonthModalProps) => {
 };
 
 
+type ToBeCreatedModel = {prj: FullProjectModel, selected: boolean};
 
-const ToBeCreated = ({projects}: {projects: FullProjectModel[]}) => {
-  const sortedProjects = projects.sort((a, b) => a.consultantName.localeCompare(b.consultantName));
-  if (!sortedProjects.length) {
+
+const ToBeCreated = ({projects, setToBeCreated}: {projects: ToBeCreatedModel[], setToBeCreated: Function}) => {
+  if (!projects.length) {
     return (
       <Row>
         <h3>{t('projectMonth.createProjects.noNewRecordsTitle')}</h3>
@@ -94,12 +109,24 @@ const ToBeCreated = ({projects}: {projects: FullProjectModel[]}) => {
     );
   }
 
+  const updateToBeCreated = (newProject: ToBeCreatedModel, selected: boolean): void => {
+    const updateIndex = projects.map(p => p.prj).indexOf(newProject.prj);
+    const newValue = projects.map((prj, idx) => updateIndex === idx ? selected : prj.selected);
+    setToBeCreated(newValue);
+  }
+
   return (
     <Row>
       <h5>{t('projectMonth.createProjects.newRecordsTitle')}</h5>
       <div className="col-12">
         <dl className="dl-box createProjectsMonth">
-          {sortedProjects.map(newProject => <ProjectRow key={newProject._id} project={newProject} />)}
+          {projects.map(newProject => (
+            <ProjectRow
+              key={newProject.prj._id}
+              project={newProject}
+              onChange={selected => updateToBeCreated(newProject, selected)}
+            />
+          ))}
         </dl>
       </div>
     </Row>
@@ -107,13 +134,15 @@ const ToBeCreated = ({projects}: {projects: FullProjectModel[]}) => {
 };
 
 
-const ProjectRow = ({project}: {project: FullProjectModel}) => {
+const ProjectRow = ({project, onChange}: {project: ToBeCreatedModel, onChange: (e: boolean) => void}) => {
   return (
     <>
-      <dt><Link claim={Claim.ViewProjects} to={`/projects/${project._id}`} label={project.consultantName} /></dt>
+      <dt>
+        <CheckboxInput value={project.selected} onChange={onChange} label={(project.prj.consultantName)} />
+      </dt>
       <dd>
-        <ProjectDurationSmall project={project.details} />
-        <div className="clientName">{project.client.name}</div>
+        <ProjectDurationSmall project={project.prj.details} />
+        <div className="clientName">{project.prj.client.name}</div>
       </dd>
     </>
   );
