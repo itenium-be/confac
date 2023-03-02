b7c2809468ee81903964229c705f16888947c741
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-03-02 04:00:55 +0100

    performance: React.memo for the ProjectMonthListCollapsed

--- frontend/src/components/project/project-month-list/OpenOrClosedProjectMonthsList.tsx
+++ frontend/src/components/project/project-month-list/OpenOrClosedProjectMonthsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import { ConfacState } from '../../../reducers/app-state';
 import { ProjectMonthListCollapsed } from './closed-list/ProjectMonthListCollapsed';
@@ -10,11 +10,21 @@ type OpenOrClosedProjectMonthsListProps = {
 };
 
 
+const ProjectMonthListCollapsedMemo = memo(({ month }: {month: string}) => {
+  // console.log(`memo rendered ${month}`, new Date().toLocaleTimeString());
+  return <ProjectMonthListCollapsed month={month} />;
+});
+
+
 export const OpenOrClosedProjectMonthsList = ({ month }: OpenOrClosedProjectMonthsListProps) => {
   const isOpen = useSelector((state: ConfacState) => state.app.filters.projectMonths.openMonths[month]);
   if (isOpen) {
     return <OpenedProjectMonthsList month={month} />;
   }
 
-  return <ProjectMonthListCollapsed month={month} />;
+  return (
+    <>
+      <ProjectMonthListCollapsedMemo month={month} />
+    </>
+  )
 };
