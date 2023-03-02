a6030d4a0ad1e47a24091acc1e4a3e07724e2072
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-03-02 04:02:45 +0100

    performance: createSelector for fixing the filters on the projectsMonth

--- frontend/src/components/project/project-month-list/open-list/OpenedProjectMonthsList.tsx
+++ frontend/src/components/project/project-month-list/open-list/OpenedProjectMonthsList.tsx
@@ -7,11 +7,40 @@ import { OpenedProjectsMonthsListToolbar } from './OpenedProjectsMonthsListToolb
 import { List } from '../../../controls/table/List';
 import { createFullProjectMonthsSelector } from '../createFullProjectMonthsSelector';
 import { ConfacState } from '../../../../reducers/app-state';
+import { createSelector } from 'reselect';
+import { ProjectMonthListFilters } from '../../../controls/table/table-models';
 
 
 type OpenedProjectMonthsListProps = {
   month: string;
 };
+
+const selectProjectMonthsFilters = (state: ConfacState) => state.app.filters.projectMonths;
+
+
+type FiltersModel = Omit<ProjectMonthListFilters, 'projectMonths'>;
+
+
+const createFiltersSelector = () => createSelector(
+  selectProjectMonthsFilters,
+  (filters) => {
+    const safeFilters: FiltersModel = {
+      ...filters,
+      openMonths: {},
+    }
+    return safeFilters;
+  }, {
+    // New in 4.1: Pass options through to the built-in `defaultMemoize` function
+    memoizeOptions: {
+      // TODO: need to update for new filters here! use deep-equal instead.
+      // --> or better, the feature has the filter method there!
+      equalityCheck: (a: FiltersModel, b: FiltersModel) => a.freeText === b.freeText && a.unverifiedOnly === b.unverifiedOnly,
+      // maxSize: 10,
+      // resultEqualityCheck: shallowEqual
+    }
+  }
+);
+
 
 
 /** A full open ProjectMonth with header + table */
@@ -19,14 +48,8 @@ export const OpenedProjectMonthsList = ({ month }: OpenedProjectMonthsListProps)
   const dispatch = useDispatch();
   const selectProjectMonths = useMemo(createFullProjectMonthsSelector, []);
   const projectMonths = useSelector((state) => selectProjectMonths(state, month));
-  const filters = useSelector((state: ConfacState) => state.app.filters.projectMonths);
-
-  const otherFilter = {
-    freeText: '',
-    showInactive: false,
-    openMonths: {},
-    unverifiedOnly: false,
-  };
+  const selectFilters = useMemo(createFiltersSelector, []);
+  const filters = useSelector((state: ConfacState) => selectFilters(state));
 
   const config: ProjectMonthFeatureBuilderConfig = {
     data: projectMonths,
