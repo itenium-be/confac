847fb433472d9e351e8fe462477a50d2290c9512
Wouter Van Schandevijl <woutervs@hotmail.com>
2023-03-08 02:40:01 +0100

    profiling: snippet: open all projectMonths automatically

--- frontend/src/reducers/app-state.ts
+++ frontend/src/reducers/app-state.ts
@@ -43,6 +43,13 @@ export const getProjectMonthsFilters = (projectMonths?: ProjectMonthModel[]): Pr
       .format(ProjectMonthsListFilterOpenMonthsFormat);
 
     openMonths = {[lastExistingMonth]: true};
+
+    // openMonths = projectMonths
+    //   .map(pm => pm.month.format(ProjectMonthsListFilterOpenMonthsFormat))
+    //   .filter((month, index, arr) => arr.indexOf(month) === index)
+    //   .reduce((acc, cur) => ({...acc, [cur]: true}), {});
+
+
     // let leMonths = ['2023-01', '2022-12', '2022-11', '2022-10', '2022-09', '2022-08']; // 6 months open
     // leMonths = ['2023-01', '2021-08', '2021-09', '2021-10', '2021-11', '2021-12', '2022-01', '2022-02', '2022-03', '2022-04', '2022-05', '2022-06', '2022-07', '2022-08', '2022-09', '2022-10', '2022-11', '2022-12'];
     // openMonths = leMonths.reduce((acc, cur) => ({...acc, [cur]: true}), {});
