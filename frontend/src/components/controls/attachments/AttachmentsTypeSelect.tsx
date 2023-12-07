6080883ea5c5973ee0b3fbb8b3472f335546b795
Nina.van.hoof <nina.van.hoof@itenium.be>
2023-12-07 10:24:10 +0100

    hiding att, adding e-mail option

--- frontend/src/components/controls/attachments/AttachmentsTypeSelect.tsx
+++ frontend/src/components/controls/attachments/AttachmentsTypeSelect.tsx
@@ -13,7 +13,7 @@ export const AttachmentsTypeSelect = ({value, onChange, ...props}: AttachmentsTy
     <StringsSelect
       value={value}
       onChange={onChange}
-      options={[...options, 'pdf']}
+      options={[...options, 'pdf', 'xml']}
       {...props}
     />
   );
