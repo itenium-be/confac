bd4cdc6b92cb4b13d0f79fdfca2f7b1789959035
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-03-09 16:08:58 +0100

    anti-perf: remove holidays cache

--- frontend/src/actions/holidays.ts
+++ frontend/src/actions/holidays.ts
@@ -1,38 +1,27 @@
 import moment from "moment";
 import Holidays from 'date-holidays';
-// import { isHoliday } from '@itenium/date-holidays-be';
-
-type WorkDaysMonthCache = {[month: string]: number};
 
 class HolidaysService {
-  cache: WorkDaysMonthCache = {};
-
   /** Returns the number of workdays in the month */
-  get(month: moment.Moment): number {
-    const monthKey = month.format('YYYY-MM');
-    if (!this.cache[monthKey])
-      this.cache[monthKey] = getWorkDaysInMonth(month);
+  get(momentInst: moment.Moment): number {
+    console.log('HolidaysService.get');
 
-    return this.cache[monthKey]
-  }
-}
+    const curMonth = momentInst.month();
+    const hd = new Holidays('BE');
 
-export function getWorkDaysInMonth(momentInst: moment.Moment): number {
-  const curMonth = momentInst.month();
-  const hd = new Holidays('BE');
-
-  const date = new Date(momentInst.year(), curMonth, 1);
-  const result: Date[] = [];
-  while (date.getMonth() === curMonth) {
-    // date.getDay = index of ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
-    if (date.getDay() !== 0 && date.getDay() !== 6) {
-      if (!hd.isHoliday(date)) {
-        result.push(date);
+    const date = new Date(momentInst.year(), curMonth, 1);
+    const result: Date[] = [];
+    while (date.getMonth() === curMonth) {
+      // date.getDay = index of ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+      if (date.getDay() !== 0 && date.getDay() !== 6) {
+        if (!hd.isHoliday(date)) {
+          result.push(date);
+        }
       }
+      date.setDate(date.getDate() + 1);
     }
-    date.setDate(date.getDate() + 1);
+    return result.length;
   }
-  return result.length;
 }
 
 export const holidaysService = new HolidaysService();
