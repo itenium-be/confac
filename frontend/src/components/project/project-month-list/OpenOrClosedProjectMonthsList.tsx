97aec1a08ce2b6730c633826e8af33469cdacbd7
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-03-09 21:02:45 +0100

    anti-perf: remove memo for ClosedProjectMonthBadges

--- frontend/src/components/project/project-month-list/OpenOrClosedProjectMonthsList.tsx
+++ frontend/src/components/project/project-month-list/OpenOrClosedProjectMonthsList.tsx
@@ -1,4 +1,3 @@
-import React, { memo } from 'react';
 import { useSelector } from 'react-redux';
 import { ConfacState } from '../../../reducers/app-state';
 import { ProjectMonthListCollapsed } from './closed-list/ProjectMonthListCollapsed';
@@ -10,21 +9,11 @@ type OpenOrClosedProjectMonthsListProps = {
 };
 
 
-const ProjectMonthListCollapsedMemo = memo(({ month }: {month: string}) => {
-  // console.log(`memo rendered ${month}`, new Date().toLocaleTimeString());
-  return <ProjectMonthListCollapsed month={month} />;
-});
-
-
 export const OpenOrClosedProjectMonthsList = ({ month }: OpenOrClosedProjectMonthsListProps) => {
   const isOpen = useSelector((state: ConfacState) => state.app.filters.projectMonths.openMonths[month]);
   if (isOpen) {
     return <OpenedProjectMonthsList month={month} />;
   }
 
-  return (
-    <>
-      <ProjectMonthListCollapsedMemo month={month} />
-    </>
-  )
+  return <ProjectMonthListCollapsed month={month} />
 };
