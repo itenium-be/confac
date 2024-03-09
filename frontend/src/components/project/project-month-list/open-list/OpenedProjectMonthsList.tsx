3ebaccb6ee8a34fee29a2ba9d3bd64eabeaf4d8f
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-03-09 20:42:30 +0100

    anti-perf: removed caching of OpenedProjectMonthList

--- frontend/src/components/project/project-month-list/open-list/OpenedProjectMonthsList.tsx
+++ frontend/src/components/project/project-month-list/open-list/OpenedProjectMonthsList.tsx
@@ -1,12 +1,11 @@
-import React, { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateAppFilters, patchProjectsMonth } from '../../../../actions';
 import { projectMonthFeature, ProjectMonthFeatureBuilderConfig } from '../../models/getProjectMonthFeature';
 import { Features } from '../../../controls/feature/feature-models';
 import { OpenedProjectsMonthsListToolbar } from './OpenedProjectsMonthsListToolbar';
 import { List } from '../../../controls/table/List';
-import { createFullProjectMonthsSelector } from '../createFullProjectMonthsSelector';
 import { ConfacState } from '../../../../reducers/app-state';
+import { useProjectsMonths } from '../../../hooks/useProjects';
 
 
 type OpenedProjectMonthsListProps = {
@@ -16,9 +15,9 @@ type OpenedProjectMonthsListProps = {
 /** A full open ProjectMonth with header + table */
 export const OpenedProjectMonthsList = ({ month }: OpenedProjectMonthsListProps) => {
   const dispatch = useDispatch();
-  const selectProjectMonths = useMemo(createFullProjectMonthsSelector, []);
-  const projectMonths = useSelector((state) => selectProjectMonths(state, month));
   const filters = useSelector((state: ConfacState) => state.app.filters.projectMonths);
+  const allProjectMonths = useProjectsMonths();
+  const projectMonths = allProjectMonths.filter(x => x.details.month.format('YYYY-MM') === month)
 
   const config: ProjectMonthFeatureBuilderConfig = {
     data: projectMonths,
