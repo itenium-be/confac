c28bc7169896882d97f064d015ce183828993656
Nicolas <nicolaslegrand009@gmail.com>
2024-12-31 14:00:44 +0100

    failing test

--- backend/src/controllers/utils/entity-events.ts
+++ backend/src/controllers/utils/entity-events.ts
@@ -3,7 +3,7 @@ import {CollectionNames, SocketEventTypes} from '../../models/common';
 import {ConfacRequest} from '../../models/technical';
 
 export function emitEntityEvent(req: ConfacRequest, eventType: SocketEventTypes, entityType: CollectionNames, entityId: ObjectID | null, entity: any|null) {
-  const sourceSocketId = req.headers['x-socket-id'];
+  const sourceSocketId = req.headers ? req.headers['x-socket-id'] : null;
   const sourceUserEmail = req.user?.data?.email;
   req.io.emit(eventType, {
     entityType,
