eb02848bcc522c2747b89138ba968cbd3aa740a7
Joris Wijnant <joris.wijnant@itenium.be>
2025-02-13 14:53:12 +0100

    feature project: filter client input based on clienttype

--- frontend/src/components/client/controls/ClientSelect.tsx
+++ frontend/src/components/client/controls/ClientSelect.tsx
@@ -3,17 +3,18 @@ import Select from 'react-select';
 import {t} from '../../utils';
 import {EnhanceInputWithLabel} from '../../enhancers/EnhanceInputWithLabel';
 import {ConfacState} from '../../../reducers/app-state';
-import {ClientModel} from '../models/ClientModels';
+import {ClientModel, ClientType} from '../models/ClientModels';
 import {SelectItem} from '../../../models';
 
 
 type ClientSelectProps = {
   /** The client _id */
   value: string | ClientModel,
+  clientType?: ClientType
   onChange: (clientId: string, client: ClientModel) => void,
 }
 
-const ClientSelectComponent = ({value, onChange}: ClientSelectProps) => {
+const ClientSelectComponent = ({value, clientType, onChange}: ClientSelectProps) => {
   const models = useSelector((state: ConfacState) => state.clients);
   const getModel = (consultantId: string): ClientModel => models.find(c => c._id === consultantId) as ClientModel;
   const getModelDesc = (c: ClientModel) => c.name;
@@ -21,6 +22,7 @@ const ClientSelectComponent = ({value, onChange}: ClientSelectProps) => {
   const selectedModelId = value && typeof value === 'object' ? value._id : value;
   const options: SelectItem[] = models
     .filter(x => x.active || x._id === selectedModelId)
+    .filter(x => clientType === undefined || x.types.includes(clientType))
     .sort((a, b) => getModelDesc(a).localeCompare(getModelDesc(b)))
     .map(item => ({value: item._id, label: getModelDesc(item)} as SelectItem));
 
