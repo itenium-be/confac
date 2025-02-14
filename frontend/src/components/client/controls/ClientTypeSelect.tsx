c1d200ff845de8d48d280220c4a9f6e891818c5e
Joris Wijnant <joris.wijnant@itenium.be>
2025-02-14 10:50:26 +0100

    feature client: change 'StringsSelect' to 'BaseSelect' to allow translations of clienttypes

--- frontend/src/components/client/controls/ClientTypeSelect.tsx
+++ frontend/src/components/client/controls/ClientTypeSelect.tsx
@@ -1,18 +1,18 @@
 import {BaseInputProps} from '../../controls/form-controls/inputs/BaseInput';
 import {ClientTypes} from '../models/ClientModels';
-import { StringsSelect } from '../../controls/form-controls/select/StringsSelect';
 import { t } from '../../utils';
+import { BaseSelect } from '../../controls/form-controls/select/BaseSelect';
 
 type ClientTypeSelectProps = BaseInputProps<string[]>
 
 
 export const ClientTypeSelect = ({ value, onChange, ...props}: ClientTypeSelectProps) => {
-
   return (
-    <StringsSelect
-      value={value}
-      onChange={onChange}
-      options={[...ClientTypes.map(ct => t(`client.clienttypes.${ct}`))]}
+    <BaseSelect
+      value={value.map(v => ({value: v, label: t(`client.clienttypes.${v}`)}))}
+      onChange={val => onChange(val.map(v => v.value))}
+      isMulti={true}
+      options={[...ClientTypes.map(ct => ({value: ct, label: t(`client.clienttypes.${ct}`)}))]}
       {...props}
     />
   );
