acf14a41f370eeca3e230e806b5b7464048c99e1
Wouter Van Schandevijl <woutervs@hotmail.com>
2025-03-16 20:44:08 +0100

    fix layout invoices list

--- frontend/src/components/invoice/invoice-table/NonGroupedInvoiceTable.tsx
+++ frontend/src/components/invoice/invoice-table/NonGroupedInvoiceTable.tsx
@@ -1,6 +1,8 @@
 import {createInvoiceList, InvoiceFeatureBuilderConfig} from '../models/getInvoiceFeature';
 import {List} from '../../controls/table/List';
 
+import './invoice-list.scss';
+
 export const NonGroupedInvoiceTable = ({config}: {config: InvoiceFeatureBuilderConfig}) => {
   const featureConfig = createInvoiceList(config);
 
